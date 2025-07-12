fbe712ccb01acee92da82a419eddbe01ef0c7176
Saurabh Sarate <106174690+Saurabhsarate@users.noreply.github.com>
2025-07-12 21:53:37 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "super-mario-tf-kubernetes-project" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-southeast-1"
-    profile = "eks"
-  }
-}
